971133fe5bdc174b0e4bad057761f9594f823d4c
Bon Enyeobi <BonCodeX@gmail.com>
2023-07-09 04:30:39 -0700

    Task 0 to 5 of Python Import and Modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 a = 1
 b = 2
 from add_0 import add as fake_add
 
-result = fake_add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+add(a, b) = fake_add(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
